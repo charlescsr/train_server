52a34a8fed5aa415a09995dfa832588edeb50626
Charles Samuel R <51647212+charlescsr@users.noreply.github.com>
2021-02-28 13:46:40 +0000

    Added function to get dataset

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+import aiofiles
+import os
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+import shutil
 
 app = FastAPI()
 models = {"lr": LinearRegression(), "lor": LogisticRegression(), "nb": GaussianNB(), "knn": KNeighborsClassifier(), 
     "dtc": DecisionTreeClassifier(), "rfc": RandomForestClassifier()}
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -26,10 +24,11 @@ async def read_root():
 async def ping():
     return {"ping": "pong"}
 
-@app.get("/model_set/{model_name}")
-async def read_item(model_name: str):
-    return {"model_name": str(models[model_name])}
+@app.post("/model_set/{model_name}/")
+async def read_item(model_name: str, data: UploadFile = File(...)):
+    location = os.environ['CS_PATH']
+    async with aiofiles.open(location+data.filename, 'wb') as dataset:
+        content = await data.read()
+        await dataset.write(content) 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"model_name": str(models[model_name]), "Result": "OK"}
