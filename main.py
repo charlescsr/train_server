27d154d1ffb9ab581097c513b6b81bdf4991e98b
Charles Samuel R <51647212+charlescsr@users.noreply.github.com>
2021-02-28 06:31:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def read_item(item_id: int, q: Optional[str] = None):
 '''
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
